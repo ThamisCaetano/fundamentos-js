80f1fbbf2e89a202488c73d5ecdf17d560ea6cd7
thamis <thamis.caetano@gmail.com>
2021-03-31 20:57:27 -0300

    added file obj

---
+++ objeto.js
@@ -0,0 +1,14 @@
+//notação literal de obj
+const prod1 = {} //um par de chaves representa um objeto
+prod1.nome = 'Celular Ultra Mega' //o objeto em JS é uma coleção de chave | valor
+prod1.preco = 4998.90
+
+console.log(prod1)
+
+//segundo exemplo 
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+console.log(prod2)
