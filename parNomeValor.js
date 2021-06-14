8e395dcef0d74f56de00def56bfd5d1a5ce75150
thamis <thamis.caetano@gmail.com>
2021-06-14 09:02:26 -0300

    made the class parNomeValor

---
+++ parNomeValor.js
@@ -0,0 +1,23 @@
+//par nome/valor
+
+const saudacao = 'Opa' //contexto léxico 1 
+
+function exec() {
+    conts saudacao = 'Faaaala' //contexto léxico 2
+    return saudacao
+}
+
+//objetos são gruos aninhados de pares nomes/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logadouro: 'Rua Muito legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
