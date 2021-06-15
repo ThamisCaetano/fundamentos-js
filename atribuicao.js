dda983a11f96c16c8baacc01b36c5910ba271d51
thamis <thamis.caetano@gmail.com>
2021-06-15 08:10:44 -0300

    made the class atribuicao & notacaoPonto

---
+++ atribuicao.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 3
+b += a //atribuição aditiva b = b + a
+console.log(b)
+
+b -= 4 //atribuição subtrativa b = b - 4
+console.log(b)
+
+b *= 2 //atribuição de multiplicação b = b * 2
+console.log(b)
+
+b /= 2 //atribuição divisiva b = b / 2
+console.log(b)
+
+b %= 2 //atribuição modular b = b % 2
+console.log(b)
